5fa679f7cd2b61ac15ea1bfd000a97a750ab869a
jamesw-li <jamesli788@gmail.com>
2025-09-23 14:38:23 -0700

    Updated AuthContext.tsx

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -80,6 +80,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   const signOut = async () => {
     await supabase.auth.signOut();
+    router.replace('/auth');
   };
 
   return (
